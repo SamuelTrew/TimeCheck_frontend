ad8489a4aa41f960fd3c80447d4ad0b38713b775
rmp17 <rmp17@ic.ac.uk>
2019-05-28 16:40:13 +0100

    change updates to be in separate vue component

--- components/GroupFeatures.vue
+++ components/GroupFeatures.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="column is-one-fifth">
-      <ul class="menu-list">
-        <div class="sidebar-item group-new group-feature">
+    <section class="section features">
+        <div class="sidebar-item group-feature">
           <b-icon icon="clock" type="is-light" size="is-medium" ></b-icon>
         </div>
-        <div class="sidebar-item group-new group-feature">
+        <div class="sidebar-item group-feature">
           <b-icon icon="poll" type="is-light" size="is-medium"></b-icon>
         </div>
-        <div class="sidebar-item group-new group-feature">
+        <div class="sidebar-item group-feature">
           <b-icon icon="map-marker" type="is-light" size="is-medium"></b-icon>
         </div>
-        <div class="sidebar-item group-new group-feature">
+        <div class="sidebar-item group-feature">
           <b-icon icon="calendar" type="is-light" size="is-medium"></b-icon>
         </div>
-        <div class="sidebar-item group-new group-feature">
+        <div class="sidebar-item group-feature">
           <b-icon icon="file-document" type="is-light" size="is-medium"></b-icon>
         </div>
-      </ul>
-    </div>
+    </section>
 </template>
 
 <script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -11,32 +11,11 @@
     </section>
 
     <section class="columns">
+      <div id="column">
       <GroupFeatures />
+      </div>
       <div class="column is-four-fifths">
-        <section class="section updates">
-          <div class="card">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image is-48x48">
-                    <img class="is-rounded" src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="media-content">
-                  <p class="title is-4">Girls Night Out</p>
-                  <p class="subtitle is-6">@reminder</p>
-                </div>
-              </div>
-              <div class="content">
-                Meeting at The Slug Club in Fulham - Don't be late!
-                <a>@TheSlugAtFulham</a>.
-                <a href="#">#css</a> <a href="#">#responsive</a>
-                <br>
-                <time datetime="2016-1-1">10:30 PM - 1 Jan 2019</time>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Updates />
       </div>
     </section>
   </section>
@@ -47,9 +26,11 @@
 
   import Taskbar from '../components/Taskbar.vue'
   import GroupFeatures from '../components/GroupFeatures.vue'
+  import Updates from "../components/Updates";
 
   export default {
     components: {
+      Updates,
       GroupFeatures,
       Taskbar,
     },
@@ -62,12 +43,5 @@
 </script>
 
 <style>
-
-  .updates{
-    background: #f7f8fb;
-    background-position: center center;
-    background-size: cover;
-    background-repeat: repeat;
-  }
 
 </style>
