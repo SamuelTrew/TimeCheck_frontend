206f4e6302e4ab22cbc68c3aa8c03d3bd86eb523
rmp17 <rmp17@ic.ac.uk>
2019-06-11 12:39:00 +0100

    beginning to implement basic note adding

--- pages/group/_groupId/notes.vue
+++ pages/group/_groupId/notes.vue
@@ -27,114 +27,63 @@
     </div>
   </div>
 
-  <div class="tile is-ancestor">
-    <div class="tile is-vertical is-9">
-      <div class="tile">
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Five</p>
-            <p class="subtitle">Subtitle</p>
-            <div class="content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
+      <div>
+        <div v-for="(line, index) in lines" v-bind:key="index" class="row">
+                <!--<div class="tile is-parent">-->
+                  <!--<article class="tile is-child box">-->
+                    <!--<p class="title">Three</p>-->
+                    <b-field horizontal v-model="line.message" label="message">
+                      <b-input type="textarea"></b-input>
+                    </b-field>
+                  <!--</article>-->
+                <!--</div>-->
+              <!--</div>-->
             </div>
-          </article>
-        </div>
-        <div class="tile is-8 is-vertical">
-          <div class="tile">
-            <div class="tile is-parent">
-              <article class="tile is-child box">
-                <p class="title">Six</p>
-                <p class="subtitle">Subtitle</p>
-              </article>
-            </div>
-            <div class="tile is-parent">
-              <article class="tile is-child box">
-                <p class="title">Seven</p>
-                <p class="subtitle">Subtitle</p>
-              </article>
-            </div>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Eight</p>
-              <p class="subtitle">Subtitle</p>
-            </article>
-          </div>
-        </div>
       </div>
-      <div class="tile">
-        <div class="tile is-8 is-parent">
-          <article class="tile is-child box">
-            <p class="title">Nine</p>
-            <p class="subtitle">Subtitle</p>
-            <div class="content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+          <div class="col-lg-2">
+            <div class="block float-right">
+              <b-button type="is-primary" @click="removeLine(index)">remove</b-button>
             </div>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Ten</p>
-            <p class="subtitle">Subtitle</p>
-            <div class="content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+            <div>
+              <b-button type="is-primary" @click="addLine"> Add </b-button>
             </div>
-          </article>
-        </div>
-      </div>
-    </div>
-    <div class="tile is-parent">
-      <article class="tile is-child box">
-        <div class="content">
-          <p class="title">Eleven</p>
-          <p class="subtitle">Subtitle</p>
-          <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-            <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
           </div>
-        </div>
-      </article>
-    </div>
-  </div>
-
-  <div class="tile is-ancestor">
-    <div class="tile is-parent">
-      <article class="tile is-child box">
-        <p class="title">Twelve</p>
-        <p class="subtitle">Subtitle</p>
-        <div class="content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.</p>
-        </div>
-      </article>
-    </div>
-    <div class="tile is-parent is-6">
-      <article class="tile is-child box">
-        <p class="title">Thirteen</p>
-        <p class="subtitle">Subtitle</p>
-        <div class="content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-        </div>
-      </article>
-    </div>
-    <div class="tile is-parent">
-      <article class="tile is-child box">
-        <p class="title">Fourteen</p>
-        <p class="subtitle">Subtitle</p>
-        <div class="content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.</p>
-        </div>
-      </article>
-    </div>
-  </div>
 
   </section>
 </template>
 
 <script>
     export default {
-        name: "notes"
+        name: "notes",
+      data () {
+        return {
+          lines: [],
+          blockRemoval: true,
+        }
+      },
+      watch: {
+        lines () {
+          this.blockRemoval = this.lines.length <= 1
+        }
+      },
+      methods: {
+        addLine () {
+
+          this.lines.push({
+            message: null,
+          })
+        },
+
+        removeLine (lineId) {
+          if (!this.blockRemoval) this.lines.splice(lineId, 1)
+        }
+      },
+      mounted () {
+        this.addLine()
+      }
     }
 </script>
+
 
 <style scoped>
 
