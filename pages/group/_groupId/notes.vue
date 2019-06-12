ddce736199c766c178eefd13f6d7f990fa92b3dd
Samuel Trew <st5317@ic.ac.uk>
2019-06-12 16:03:04 +0100

    Reformatted Notes to follow a dynamic style

--- pages/group/_groupId/notes.vue
+++ pages/group/_groupId/notes.vue
@@ -1,30 +1,39 @@
 <template>
-  <section>
-  <div class="tile is-ancestor">
-    <div class="tile is-parent">
+  <section class="section updates">
+
+    <div class="columns is-multiline">
       <div v-for="(line, index) in lines" v-bind:key="index" class="row">
-      <article class="tile is-child box">
-        <p class="title">hello</p>
-        <p class="subtitle">{{line.date.toDateString()}}</p>
-        <p class="subtitle">{{line.date.toTimeString()}}</p>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title">Hello</p>
+                  <p class="subtitle">{{line.date.toDateString()}}</p>
+                  <p class="subtitle">{{line.date.toTimeString()}}</p>
+                </div>
+              </div>
+              <div class="content">
+                <b-field horizontal v-model="line.message" label="message">
+                  <b-input type="textarea"></b-input>
+                </b-field>
 
-        <b-field horizontal v-model="line.message" label="message">
-          <b-input type="textarea"></b-input>
-        </b-field>
+                <div class="block float-right">
+                  <b-button type="is-primary" @click="removeLine(index)">Remove Note</b-button>
+                </div>
+              </div>
+            </div>
 
-        <div class="block float-right">
-          <b-button type="is-primary" @click="removeLine(index)">Remove Note</b-button>
+          </div>
         </div>
-
-      </article>
       </div>
     </div>
+
+  <div class="col-lg-2">
+    <div>
+      <b-button type="is-primary" @click="addLine"> Add Note </b-button>
+    </div>
   </div>
-          <div class="col-lg-2">
-            <div>
-              <b-button type="is-primary" @click="addLine"> Add Note </b-button>
-            </div>
-          </div>
 
   </section>
 </template>
