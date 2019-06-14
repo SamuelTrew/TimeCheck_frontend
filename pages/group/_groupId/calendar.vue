5710ac38820e428c7c3af7ff02a06efc990ae9a5
Hao  Liang <hao.liang17@imperial.ac.uk>
2019-06-14 15:39:22 +0100

    Simplified Calendar

--- pages/group/_groupId/calendar.vue
+++ pages/group/_groupId/calendar.vue
@@ -6,9 +6,22 @@
       <div class="calendar-page-container">
         <div class="calendar-container">
           <p class="calendar-sub-title">
-            Group
+            Select dates when you are busy:
           </p>
-          <p> Dates when people are busy </p>
+          <v-date-picker
+            :available-dates='availableDates'
+            :value="null"
+            color="red"
+            is-expanded
+            is-inline
+            mode='multiple'
+            v-model='pickedDates'></v-date-picker>
+        </div>
+
+        <div class="calendar-container">
+          <p class="calendar-sub-title">
+            Dates when people are busy
+          </p>
           <v-date-picker
             :available-dates='{end: new Date(1, 1, 1)}'
             :value='sharedDates'
@@ -17,30 +30,13 @@
             is-inline
             mode='multiple'></v-date-picker>
         </div>
-
-        <div class="calendar-container">
-          <p class="calendar-sub-title">
-            Personal
-          </p>
-          <p> Select dates when you are busy </p>
-          <v-date-picker
-            :available-dates='availableDates'
-            :value="null"
-            color="red"
-            is-dark
-            is-expanded
-            is-inline
-            mode='multiple'
-            v-model='pickedDates'></v-date-picker>
-        </div>
       </div>
     </section>
 
     <section class="calendar-settings">
-      <div class="calendar-title">Calendar Settings</div>
-
       <div>
         <div class="calendar-sub-title">Available dates range</div>
+        Set the potential dates people can pick from:
         <div class="calendar-range-picker">
           <v-date-picker
             mode='range'
@@ -67,14 +63,16 @@
         startDate: new Date(),
         endDate: new Date(),
         availableDates: null,
-        pickedDates: [
+        pickedDates: [],
+        pickedDatesDummy: [
           new Date('6/10/2019'),
           new Date('6/11/2019'),
           new Date('6/12/2019'),
           new Date('6/13/2019'),
           new Date('6/14/2019'),
         ],
-        groupDates: [
+        groupDates: [],
+        groupDatesDummy: [
           new Date('6/6/2019'),
           new Date('6/7/2019'),
           new Date('6/13/2019'),
